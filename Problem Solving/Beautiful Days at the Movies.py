c81cbeb3aa97e841105b272207f0ef2cd3d0743d
Ishita Sharma <92918963+ishita-sh18@users.noreply.github.com>
2022-08-23 19:15:37 +0530

    Create Beautiful Days at the Movies.py

---
+++ Problem Solving/Beautiful Days at the Movies.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    count=0
+    for i in range(i,j+1):
+        org=i
+        Reverse = 0    
+        while(i > 0):    
+            Reminder = i %10    
+            Reverse = (Reverse *10) + Reminder    
+            i = i //10 
+        revnum=Reverse
+        if((abs(org-revnum)/k)==round((abs(org-revnum)/k))):
+            count+=1
+    return count 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
