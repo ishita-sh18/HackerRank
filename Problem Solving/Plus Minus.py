2f3cad73c7b314225718438aecf79131d9c3e4cf
Ishita Sharma <92918963+ishita-sh18@users.noreply.github.com>
2022-08-23 18:46:41 +0530

    Create Plus Minus.py

---
+++ Problem Solving/Plus Minus.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+    neg,zero,pos=0,0,0
+    for i in range(len(arr)):
+        if(arr[i]<0):
+            neg+=1 
+        elif(arr[i]==0):
+            zero+=1 
+        else:
+            pos+=1 
+    print(pos/len(arr))
+    print(neg/len(arr))
+    print(zero/len(arr))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
