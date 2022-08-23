dc8feb135d0b7bfd36d3102e18a5bbda6f9aa756
Ishita Sharma <92918963+ishita-sh18@users.noreply.github.com>
2022-08-23 18:47:58 +0530

    Create Mini-Max Sum.py

---
+++ Problem Solving/Mini-Max Sum.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    arr.sort()
+    print(sum(arr[:(len(arr)-1)]),end=" ")
+    print(sum(arr[1:len(arr)]))
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
